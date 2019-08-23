d008cf4f46746f90574bf06d9e3f64c7e3ca4626
Brad Mortensen <b.mort86@gmail.com>
2019-08-23 10:22:25 -0600

    Logics playlist controller

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class PlaylistsController < ApplicationController
       def index
-        @playlists = User.find(params[:user_id]).playlists.all()
+        @playlists = Playlist.where(:ser_id: params[:user_id])
 
         render json: @playlists
       end
@@ -14,13 +14,14 @@ module Api
       end
 
       def create
-        
+        @user = User.find(params[:user_id])
+        @user_playlist = @user.user_playlists.build(user_playlist_params)
       end
 
       private
 
-      def user_params
-        params.require(:user).permit(:email)
+      def user_playlist_params
+        params.require(:playlist).permit(:playlist_id)
       end
     end
   end
